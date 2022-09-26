6d571d9e449ca80c63f67c4a2c37b74a738e5416
HoKin <113962377+HoKin456@users.noreply.github.com>
2022-09-26 23:42:24 +0800

    Add files via upload
    宏定义实现

--- Larger_values.c
+++ Larger_values.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* 函数实现
 int LargeValues(int x , int y)
 {   
     int max;
@@ -8,14 +9,18 @@ int LargeValues(int x , int y)
     printf("较大的数为:%d\n\n",max);
     return 0;
 }
+*/
+#define LargerValues(x,y) (x>y?x:y) // 宏的定义
 
 int main()
 {
     int num1;
     int num2;
+    int larger_values;// 宏定义函数需要
     printf("请输入两个整数:\n");
     scanf("%d %d",&num1,&num2);
-    LargeValues(num1,num2);
+    int larger_values = LargerValues(num1,num2);
+    printf("较大值为:%d\n\n",larger_values);
     system("pause");
     return 0;
 }
