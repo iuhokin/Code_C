a342414f8a47617e35842e8102dab61aaf89252b
HoKin <113962377+HoKin456@users.noreply.github.com>
2022-09-28 20:02:29 +0800

    Add files via upload
    重复打印

--- Repeat_Print.c
+++ Repeat_Print.c
@@ -4,8 +4,8 @@
 int main()
 {
     char ch;
-    while((ch = getchar()) != EOF)//EOF->ctrl + z 要注意(ch = getchar()),符号运算级问题,建议都加()
-    {
+    while((ch = getchar()) != EOF)// EOF->ctrl + z 要注意(ch = getchar()),符号运算级问题,建议都加()
+    {                             // EOF->End of File (-1)
         putchar(ch);
     }
     return 0;
