508aa1bd16c78d00bb3e87fd2ec00eab8da218f0
HoKin <113962377+HoKin456@users.noreply.github.com>
2022-10-09 23:31:11 +0800

    Create test.c
    yyyy

---
+++ .github/workflows/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    for(i = 1;i <= 100;i++)
+    {
+        if(i % 3 == 0)
+        {
+            printf("%d\n",i);
+        }
+    }
+    system("pause");
+    return 0;
+}
