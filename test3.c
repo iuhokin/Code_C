5dc689a449dd8d14a058c946b0e0273c500a2ec6
HoKin <113962377+HoKin456@users.noreply.github.com>
2022-10-18 22:51:44 +0800

    Add files via upload

--- test3.c
+++ test3.c
@@ -4,12 +4,7 @@
 int main()
 {
     int i;
-    int arry[] = {1,2,3,4,5,6,7,8,9,10};
-    int len = sizeof(arry) / sizeof(arry[0]);
-    for(i = 0;i < len;i++)
-    {
-        printf("&arry[%d] = %p\n",i,&arry[i]);
-    }
+    printf("你好");
     system("pause");
     return 0;
 }
